677e16bf5989b5c39c2a8263d615449d30a16925
Vladimir <101038833+postmodernist1488@users.noreply.github.com>
2022-03-11 03:39:51 +0300

    Add files via upload

--- stepik.py
+++ stepik.py
@@ -1,11 +1,4 @@
-n, m = [int(i) for i in input().split()]
-matrix = [[0] * m for _ in range(n)]
-for i in range(n):
-    for j in range(m):
-        matrix[i][j] = i + j + 1
+set1 = frozenset('beegeek')
+set2 = set('stepik')
 
-for row in matrix:
-    for el in row:
-        print(str(el).ljust(3), end = ' ')
-    print()
-    
+print(set2 | set1)
