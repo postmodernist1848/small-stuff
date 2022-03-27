9d1cc7fef78e1d6755d1507f6e64dbe41379f3f4
postmodernist1488 <elitic.pantheism@gmail.com>
2022-03-27 17:32:36 +0300

    did google-search-2

--- stepik.py
+++ stepik.py
@@ -9,4 +9,5 @@ print(*reversed(max([(sum(j for j in range(1, i + 1) if i % j == 0), i) for i in
 (lambda c: print('оранжевый' if c == ['желтый', 'красный'] else 'фиолетовый' if c == ['красный', 'синий'] else 'зеленый' if c == ['желтый', 'синий'] else c[0] if c[0] in ['красный', 'желтый', 'синий'] and c[0] == c[1] else 'ошибка цвета'))(sorted([input(), input()])) 
 #k-ая буква слова
 [[print(x[k], end='') for x in lst if len(x) >= k + 1] for lst, k in [([input() for _ in range(int(input()))], int(input()) - 1)]]
-
+#Google-search-2
+(lambda data, querries: print(*[s for s in data if all([q.lower() in s.lower() for q in querries])], sep='\n'))([input() for _ in range(int(input()))], [input() for _ in range(int(input()))])
